ef0f9c5e65045a68ca3de0611c3002d99bbd2436
parthmern <parthmern@gmail.com>
2024-11-11 22:51:02 -0500

    prod bug1

--- src/producers/evaluationQueueProducer.ts
+++ src/producers/evaluationQueueProducer.ts
@@ -3,5 +3,5 @@ import evaluationQueue from "../queues/evaluationQueue";
 
 export default async function(payload: Record<string, unknown>) {
     await evaluationQueue.add("EvaluationJob", payload);
-    console.log("Successfully added a new evaluationQueue job");
+    console.log("Successfully added a new evaluationQueue job", payload);
 }
