4360bfd5918119f417f0554b784b4b049aa67fbc
parthmern <parthmern@gmail.com>
2025-01-02 18:53:39 -0500

    fix: cicd redis pswd3

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -5,11 +5,21 @@ import ServerConfig from './serverConfig';
 console.log(ServerConfig.REDIS_PORT,ServerConfig.REDIS_HOST, ServerConfig.REDIS_PASSWORD);
 
 const redisConfig = {
-    port:ServerConfig.REDIS_PORT,
+    port: ServerConfig.REDIS_PORT,
     host: ServerConfig.REDIS_HOST,
-    password : ServerConfig.REDIS_PASSWORD,
-    maxRetriesPerRequest: null 
+    password: ServerConfig.REDIS_PASSWORD,
+    maxRetriesPerRequest: null, // Allow the custom retry strategy to manage retries
+    retryStrategy: (times:any) => {
+        if (times >= 10) {
+            console.error('Redis connection attempts exceeded 10 retries.');
+            return null; // Stop retrying
+        }
+        const delay = Math.min(times * 100, 2000); // Incremental delay, max 2 seconds
+        console.log(`Retrying Redis connection in ${delay}ms... (Attempt ${times})`);
+        return delay;
+    },
 };
+
 
 const redisConnection = new Redis(redisConfig);
 
