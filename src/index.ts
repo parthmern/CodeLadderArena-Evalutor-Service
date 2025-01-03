e595f4b0031120e3fbbeecec4dfc474db985b4d6
parthmern <parthmern@gmail.com>
2025-01-02 19:16:34 -0500

    hardcode pswd.

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use('/bullboardui', serverAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   console.log(`chnaged server started running on- ${serverConfig.PORT} on ${serverConfig.REDIS_PASSWORD}`, serverConfig.REDIS_PORT,serverConfig.REDIS_HOST, serverConfig.REDIS_PASSWORD);
-  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/bullboardui`, serverConfig.REDIS_PORT,serverConfig.REDIS_HOST, serverConfig.REDIS_PASSWORD);
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/bullboardui `, serverConfig.REDIS_PORT,serverConfig.REDIS_HOST, serverConfig.REDIS_PASSWORD);
   console.log(serverConfig.REDIS_PORT,serverConfig.REDIS_HOST, serverConfig.REDIS_PASSWORD);
 
   SampleWorker('SampleQueue');
